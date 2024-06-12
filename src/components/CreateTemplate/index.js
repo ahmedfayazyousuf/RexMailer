5284eb0e027a1fd12e58e82484ec035def94a711
ahmedfayazyousuf <ahmedfayazyousuf@gmail.com>
2024-06-13 00:02:04 +0400

    o
    o

--- src/components/CreateTemplate/index.js
+++ src/components/CreateTemplate/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import '../1_MediaAssets/Styles/All.css';
 import EmailEditor from '../EmailEditor/index.js';
-import Cover from '../1_MediaAssets/Home/Texture.png'
 
 const CreateTemplate = () => {
     return (
-        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100vw', height: '100%', background: 'white', color: 'black', paddingTop: '50px', backgroundImage: `url('${Cover}')`, backgroundPosition: 'center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover'}}>
-            <div style={{width: '80vw', background: 'white', padding: '20px', borderRadius: '10px'}}>
-            <h1>Welcome to REX Mailer!</h1>
+        <div className='MainDiv' style={{paddingTop: '70px'}}>
+            
+            <h2 style={{textAlign: 'center', margin: '0'}}><span>Create an </span><span style={{color: '#FF3380', fontWeight: '900'}}>Email Template</span></h2>
+            
+            <div className='EditorDiv'>
                 <EmailEditor />
             </div>
         </div>
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,18 +5,16 @@ import React from 'react';
 import '../1_MediaAssets/Styles/All.css';
 import Illustration1 from '../1_MediaAssets/Home/Illustration1.png';
 import Illustration2 from '../1_MediaAssets/Home/Illustration2.png';
-import Cover from '../1_MediaAssets/Home/Texture2.png';
 import { NavLink } from "react-router-dom";
 
 const Home = () => {
     return (
-        // backgroundImage: `url('${Cover}')`, backgroundPosition: 'center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundAttachment: 'fixed'
-        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', width: '100vw', height: '100vh', background: '#fff', color: 'black', position: 'relative', backgroundImage: `url('${Cover}')`, backgroundPosition: 'center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundAttachment: 'fixed'}}>
+        <div className="MainDiv" style={{height: '100vh'}}>
             
-            <h1><span>Welcome to the </span><span className="fontSpecial" style={{color: '#FF3380'}}>REX Mailer!</span></h1>
-            <div style={{ padding: '0px', borderRadius: '10px', display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '10px', flexWrap: 'wrap'}}>
-                <img src={Illustration1} alt="lalaland" style={{position: 'absolute', bottom: '0', right: '0', width: '250px'}}/>
-                <img src={Illustration2} alt="lalaland" style={{position: 'absolute', top: '50px', left: '0', width: '230px'}}/>
+            <h2 style={{textAlign: 'center'}}><span>Welcome to the </span><span style={{color: '#FF3380', fontWeight: '900'}}>REX Mailer!</span></h2>
+            <div style={{ padding: '0px', borderRadius: '10px', display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '10px', flexWrap: 'wrap', margin: '0px 10px 0px 10px'}}>
+                <img className="DPhider" src={Illustration1} alt="lalaland" style={{position: 'absolute', bottom: '0', right: '0', width: '250px'}}/>
+                <img className="DPhider" src={Illustration2} alt="lalaland" style={{position: 'absolute', top: '50px', left: '0', width: '230px'}}/>
 
                 
                 <NavLink to="/CreateTemplate" style={{textDecoration: 'none'}}>
