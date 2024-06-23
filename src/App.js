1a76729b4a5340c16a5ca7bf6032629b0b349861
ahmedfayazyousuf <ahmedfayazyousuf@gmail.com>
2024-06-23 12:41:10 +0400

    o
    o

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import CreateTemplate from './components/EmailTemplates/CreateTemplate';
 import SendEmails from './components/SendEmails';
-import AddressBooks from './components/Contacts/AddressBooks.js';
+import Contacts from './components/Contacts/Contacts.js';
 import AddressBook from './components/Contacts/AddressBook.js';
 import EmailTemplates from './components/EmailTemplates';
 import Navbar from './components/Constants/Navbar';
@@ -20,7 +20,7 @@ function App() {
         <Route path="/CreateTemplate" element={<CreateTemplate />} />
         <Route path="/SendEmails" element={<SendEmails />} />
         <Route path="/EmailTemplates" element={<EmailTemplates />} />
-        <Route path="/AddressBooks" element={<AddressBooks />} />
+        <Route path="/Contacts" element={<Contacts />} />
         <Route path="/AddressBook/:id" element={<AddressBook />} />
       </Routes>
     </Router>
